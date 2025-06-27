dfc9229599ebb26629c7fad22a41d0cb3a699d64
Ravi-Katiyar04 <drxravikatiyar0000@gmail.com>
2025-06-27 16:11:46 +0530

    authentication

---
+++ src/utils/authMiddleware.js
@@ -0,0 +1,26 @@
+import jwt from 'jsonwebtoken';
+import User from '@/models/user';
+import dbConnect from './dbconnect';
+
+export default async function authenticate(req) {
+  await dbConnect();
+
+  const token = req.cookies.get('token')?.value;
+
+  if (!token) {
+    throw new Error('Not authenticated');
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findById(decoded.id).select('-password');
+
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    return user; // return authenticated user object
+  } catch (err) {
+    throw new Error('Invalid token');
+  }
+}
