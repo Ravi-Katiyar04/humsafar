f0dfee32b17bb6b570f09e60946c067896ccbb35
Ravi-Katiyar04 <drxravikatiyar0000@gmail.com>
2025-08-18 23:27:09 +0530

    SearchStation and TrainsBetweenStations route created

---
+++ src/app/api/searchStation/route.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+
+export async function GET(req) {
+    const { searchParams } = new URL(req.url);
+    const query = searchParams.get('query');
+
+    if (!query) {
+        return Response.json({ error: 'No query provided.' }, { status: 400 });
+    }
+
+    const options = {
+        method: 'GET',
+        url: 'https://irctc1.p.rapidapi.com/api/v1/searchStation',
+        params: {
+            query: query
+        },
+        headers: {
+            'x-rapidapi-key': process.env.RAPIDAPI_KEY,
+            'x-rapidapi-host': 'irctc1.p.rapidapi.com'
+        },
+    };
+
+    try {
+        const response = await axios.request(options);
+        if (!response.data || response.data.length === 0) {
+            return Response.json({ error: 'Train not found or no data available.' }, { status: 404 });
+        }
+        return Response.json(response.data.data, { status: 200 });
+    } catch (error) {
+        console.error('Train Status Error:', error);
+        return Response.json({ error: 'Train not found or no data available.' }, { status: 500 });
+    }
+}
---
+++ src/app/api/trainBetweenStations/route.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+export async function GET(req) {
+    const { searchParams } = new URL(req.url);
+    const trainNumber = searchParams.get('trainNumber');
+
+    const options = {
+        method: 'GET',
+        url: 'https://irctc1.p.rapidapi.com/api/v3/trainBetweenStations',
+        params: {
+            fromStationCode: trainNumber,
+            toStationCode: '1',
+            dateOfJourney: '2023-10-01' 
+        },
+        headers: {
+            'x-rapidapi-key': process.env.RAPIDAPI_KEY,
+            'x-rapidapi-host': 'irctc1.p.rapidapi.com'
+        },
+    };
+
+    try {
+        const response = await axios.request(options);
+        if (!response.data || response.data.length === 0) {
+            return Response.json({ error: 'Train not found or no data available.' }, { status: 404 });
+        }
+        return Response.json(response.data, { status: 200 });
+    } catch (error) {
+        console.error('Train Status Error:', error);
+        return Response.json({ error: 'Train not found or no data available.' }, { status: 500 });
+    }
+}
